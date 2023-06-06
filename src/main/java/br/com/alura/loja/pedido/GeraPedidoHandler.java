95e8b9cd41b06201e41253a33dfba6b4050d2e5f
luizpereirarj <33535349+luizpereirarj@users.noreply.github.com>
2023-06-05 23:00:06 -0300

    Observer Example

--- src/main/java/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/main/java/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,18 +1,29 @@
 package br.com.alura.loja.pedido;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
 
-    // construtor com injeção de dependênias: repository, service etc.
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
 
     public void execute(GeraPedido dados){
         Orcamento orcamento = new Orcamento(dados.getValorOrcamento(),dados.getQuantidadeDeItens());
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 
-        System.out.println("Salvar o pedido no banco de dados.");
-        System.out.println("Salvar email com dados do novo pedido.");
+        SalvarPedidoNoBancoDeDados salvarPedidoNoBancoDeDados= new SalvarPedidoNoBancoDeDados();
+        EnviarEmailPedido enviarEmailPedido=new EnviarEmailPedido();
+
+       acoes.forEach(a->a.executarAcao(pedido));
+
     }
 }
